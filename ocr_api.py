a18f8ab606245c78e4d24baa1ffec797ec794ea0
梁锐 <liangrui@sinohealth.cn>
2020-12-21 10:50:54 +0800

    修正相关代码。

--- ocr_api.py
+++ ocr_api.py
@@ -32,24 +32,35 @@ def get_info(path):
         data = rs["data"]
         # 这里还得需要一个信息显示的排序
         data_tmp = sorted(data["raw_out"], key=lambda x: x[0][1])
+        # print(data_tmp)
         old_v = 0
         h = 0
         row_data = []
         for i, row in enumerate(data_tmp):
+            # 首行
             # (x, y, w, h, 角度)
             if i == 0:
                 old_v = row[0][1]
                 h = row[0][3]
                 continue
-            if abs(row[0][1] - old_v) > h * 0.85:
+
+            # 当遇到新行了
+            # print((row[0][1] + row[0][1] * 0.5),old_v + h * 0.5)
+            if abs((row[0][1] + row[0][3] * 0.5) - (old_v + h * 0.5)) >= (row[0][3] + h) * 0.5 * 0.5:
                 if len(row_data) > 0:
                     row_data = sorted(row_data)
                     info.append(row_data)
-                row_data = []
+                    row_data = []
+
             old_v = row[0][1]
             h = row[0][3]
             # print(row[1], end=" ")
             row_data.append((row[0][0], row[1]))
+        # 把最后一行信息加入其中
+        if len(row_data) > 0:
+            row_data = sorted(row_data)
+            info.append(row_data)
+            row_data = []
         print("时间:", data['speed_time'])
         # 显示处理图片
         base64_data = data["img_detected"].replace("data:image/jpeg;base64,", "")
@@ -57,7 +68,7 @@ def get_info(path):
         nparr = np.fromstring(imgData, np.uint8)
         img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         cv2.imwrite("./pics/_%s" % (os.path.split(path)[-1]), img_np)
-        return data['raw_out']
+        return info
     else:
         return None
         # cv2.imshow('IMG', img_np)
@@ -65,6 +76,8 @@ def get_info(path):
         # cv2.destroyAllWindows()
 
 
-infos = get_info("./pics/0000.png")
+infos = get_info("./pics/050.png")
 for info in infos:
-    print(info[1])
+    for xsrc in info:
+        print(xsrc[1], end=" ")
+    print()
