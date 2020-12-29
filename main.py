c698c7b40a11059c477c696781c66f504afd06e9
liangrui <liangruiveryhappy@163.com>
2020-12-29 22:52:46 +0800

    增加小小的测试。

--- main.py
+++ main.py
@@ -66,35 +66,37 @@ if __name__ == '__main__':
     list1 = main_app.child_window(title_re="List1", class_name="SysListView32")
     items = list1.items()
     c_count = list1.column_count()
-    cur_c = 1
-    for i, item in enumerate(items):
-        print(item['text'])
-        print(item)
-        cur_c += 1
-        print(item.GetItem(), end=" ")
-        if cur_c > c_count:
-            print()
-            cur_c = 1
+    ############### 这个是测试用
+    # cur_c = 1
+    # for i, item in enumerate(items):
+    #     # print(item['text'])
+    #     # print(item)
+    #     cur_c += 1
+    #     # print(item.GetItem(), end=" ")
+    #     if cur_c > c_count:
+    #         print()
+    #         cur_c = 1
+    ############### 这个是测试用
     pre_count = list1.item_count()
     n_total_count += 1
     print("第%s次刷新，共有%s条记录." % (n_total_count, pre_count))
     while True:
-        time.sleep(5)
+        time.sleep(2)
         main_app = app["国海证券金探号超级终端V7.00"]
         main_app['刷新'].click()
         list1 = main_app.child_window(title_re="List1", class_name="SysListView32")
         c_count = list1.item_count()
         n_total_count += 1
         print("第%s次刷新，共有%s条记录." % (n_total_count, pre_count))
-        # if c_count > pre_count:
-        print("记录有更新.")
-        sound_thread = PalySound()
+        if c_count > pre_count:
+            print("记录有更新.")
+            sound_thread = PalySound()
         # sound_thread.setDaemon(True)
-        sound_thread.start()
-        a = input("请输入任意值继续...")
-        sound_thread.terminate()
-        # else:
-        #     print("记录不变.")
+            sound_thread.start()
+            a = input("请输入任意值继续...")
+            sound_thread.terminate()
+        else:
+            print("记录不变.")
         pre_count = c_count
 
 
