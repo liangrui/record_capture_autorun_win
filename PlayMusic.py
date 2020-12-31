450b02d89f254b55507a9f1339574adb526a016e
liangrui <liangruiveryhappy@163.com>
2020-12-31 23:32:57 +0800

    增加交替的声音。

--- PlayMusic.py
+++ PlayMusic.py
@@ -1,12 +1,14 @@
 import threading
 import winsound
-
+import time
 
 class PalySound(threading.Thread):
-    def __init__(self):
+    def __init__(self,sound='./sounds/3.wav'):
         print("初始化...")
         threading.Thread.__init__(self)
         self._running = True
+        self._sound = sound
+
 
     def terminate(self):
         self._running = False
@@ -14,6 +16,9 @@ class PalySound(threading.Thread):
     def run(self):
         self.play_sound()
 
-    def play_sound(self, sound='./sounds/3.wav'):
-        while self._running:
-            winsound.PlaySound(sound, winsound.SND_ALIAS)
+    def play_sound(self):
+        t1 = t2 = time.time()
+        while self._running and (t2 - t1) < 10:
+            print(t2-t1)
+            winsound.PlaySound(self._sound, winsound.SND_ALIAS)
+            t2 = time.time()
